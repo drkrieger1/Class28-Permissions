f42695d691a5c7b53138b137320ba51a662279ad
drkrieger <eplyushko@gmail.com>
2017-10-19 18:41:11 -0700

    controlles mad with home controller, models folder and views folder with home view folder

---
+++ Lab28Erik/Lab28Erik/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab28Erik.Controllers
+{
+    public class HomeController
+    {
+    }
+}
