2bf5f63587439e8e1a7c5b6deacff8d7c6c57c21
drkrieger <eplyushko@gmail.com>
2017-10-19 19:14:23 -0700

    applicationDbcontext added

--- Lab28Erik/Lab28Erik/Models/LogInViewModel.cs
+++ Lab28Erik/Lab28Erik/Models/LogInViewModel.cs
@@ -7,5 +7,13 @@ namespace Lab28Erik.Models
 {
     public class LogInViewModel
     {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        public bool RememberMe { get; set; }
     }
 }
