d53866de9c3e75528ae574ae7c10d06088eb36e2
drkrieger <eplyushko@gmail.com>
2017-10-24 09:56:59 -0700

    reroute fix found

--- Lab28Erik/Lab28Erik/Controllers/AccountsController.cs
+++ Lab28Erik/Lab28Erik/Controllers/AccountsController.cs
@@ -79,7 +79,7 @@ namespace Lab28Erik.Controllers
             ModelState.AddModelError("", error);
             return View();
         }
-        //[AllowAnonymous]
+        [AllowAnonymous]
         [HttpGet]
         public IActionResult AdminRegister(string returnUrl = null)
         {
